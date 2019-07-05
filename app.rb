795c484620f99b0c5c00017f4351ce299107e1f2
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-07-05 02:32:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,19 +2,18 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   configure do
-    enable :sessions
-    set :session_secret, "secret"
-  end
-
-  get '/' do
-    @session = session[:session_secret]
-    erb :index
-  end
-
-  post '/checkout' do
-    session[:item] = params[:item]
-    @session = session
-    erb :checkout
-  end
-
-end
+      enable :sessions
+      set :session_secret, "secret"
+    end
+    get '/' do
+      @session = session[:session_secret]
+      erb :index
+    end
+  
+  
+  post '/checkout' do 
+      session[:item] = params[:item]
+      @session = session
+      erb :checkout
+    end
+  end 
